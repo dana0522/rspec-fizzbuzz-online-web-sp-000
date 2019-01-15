bbd7f203eecd67cb082865dfb2e888ac3a2382c2
dana kim <dana0522@live.com>
2019-01-15 03:24:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(x)
+  if x % 3==0
+    puts "Fizz"
+  elsif x % 5==0
+    puts "Buzz"
+  elsif x%4==0
+    return nil
+  elsif x % 5==0 and x % 3==0
+    puts "FizzBuzz"
+end
