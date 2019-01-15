7fa6b1f94016467f5b7d7839e93fc424830fe958
dana kim <dana0522@live.com>
2019-01-15 03:54:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(x)
-  if x % 5 == 0 && x % 3 == 0
-    puts "FizzBuzz"
-  elsif x % 3 == 0
-    puts "Fizz"
-  elsif x % 5 == 0
-    puts "Buzz"
-  elsif x % 4 == 0
-    return nil
-  end
+def fizzbuzz(i)
+    if (i % 5) == 0 && (i % 3) ==0
+        puts 'FizzBuzz'
+    elsif (i % 5) == 0
+        puts 'Buzz'
+    elsif (i % 3) == 0
+        puts 'Fizz'
+    else
+        puts i
+    end
 end
-
